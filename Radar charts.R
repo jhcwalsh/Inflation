96c568ff70e86a67db937eed1f8b29b2f261f2c4
jhcwalsh <james@walsh.nu>
2021-03-28 15:48:51 -0700

    Spider chart demo Import data

---
+++ Radar charts.R
@@ -0,0 +1,51 @@
+# Library
+library(fmsb)
+
+# Create data: note in High school for Jonathan:
+data <- as.data.frame(matrix( sample( 2:20 , 20 , replace=T) , ncol=10))
+colnames(data) <- c("maths" , "english" , "biology" , "music" , "R-coding", "data-viz" , "french" , "physics", "statistics", "sport" )
+
+# To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
+data <- rbind(rep(20,10) , rep(0,10) , data)
+
+# Check your data, it has to look like this!
+# head(data)
+
+# The default radar chart 
+radarchart(data)
+
+#pcol=line color
+#pfcol=fill color
+#plwd=line width
+#cglcol=color of the net
+#cglty=net line type
+
+radarchart( data  , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
+
+
+# Color vector
+colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+
+# plot with default options:
+radarchart( data  , axistype=1 , 
+            #custom polygon
+            pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
+            #custom labels
+            vlcex=0.8 
+)
+
+# Add a legend
+legend(x=0.7, y=1, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
